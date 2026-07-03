59343b5d2290bf2f9f65a3ed9317d34c9842b0b7
tamil243 <tamil243@outlook.com>
2026-07-03 12:17:38 +0530

    Fix hydration errors with dynamic imports and SSR disabled

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import '@/styles/globals.css';
-import AnimatedBackground from '@/components/AnimatedBackground';
+
+const AnimatedBackground = dynamic(() => import('@/components/AnimatedBackground'), {
+  ssr: false,
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
